f45413a047149eef187c44bcce0e354a50a9050f
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-03 15:37:22 +0530

    day 17

---
+++ day 017/main.py
@@ -0,0 +1,16 @@
+name = 'Abhishek'
+for i in name:
+    print(i, end=", ")
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for x in colors:
+    print(x)
+
+for k in range(5):
+    print(k)
+
+for k in range(4,9):
+    print(k)
+
+for k in range(1,100,2):
+    print(k)
