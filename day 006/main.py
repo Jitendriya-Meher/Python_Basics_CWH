fec98c6de66e42e5a4495ae97a76d576aec65276
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-03 12:09:48 +0530

    adding day 6

---
+++ day 006/main.py
@@ -0,0 +1,31 @@
+a = 1
+print("a = ",a)
+print("type of a is",type(a))
+
+b = 34.43
+print("b = ",b)
+print("type of b is",type(b))
+
+c = "jiten"
+print("c = ",c)
+print("type of c is",type(c))
+
+d = True
+print("d = ",d)
+print("type of d is",type(d))
+
+e = complex(3,4)
+print("e = ",e)
+print("type of e is",type(e))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print("list =",list1)
+print("type of list is",type(list1))
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print("tuple =",tuple1)
+print("type of tuple is",type(tuple1))
+
+dict1 = {"name":"Jiten", "age":20, "canVote":True}
+print("dict =",dict1)
+print("type of dict is",type(dict1))
