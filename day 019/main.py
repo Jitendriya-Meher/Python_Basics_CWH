fb1705bf5b145e56aec574486a5862548d4a93f3
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-03 16:12:39 +0530

    day 19

---
+++ day 019/main.py
@@ -0,0 +1,11 @@
+for i in range(1,101,1):
+    print(i ,end=" ")
+    if(i==50):
+        break
+    else:
+        if(i<10):
+            print(i ,end=" ")
+            continue
+        print("Mississippi")
+        
+print("Thank you")
