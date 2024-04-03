a6c480c932d0b9b29a6d4536dc412de0a6d73b28
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-03 11:50:11 +0530

    adding day 5

---
+++ day 005/main.py
@@ -0,0 +1,17 @@
+# print your name - comments in python
+print("hii\n \'my name is\' \t \"jitendriya\"")
+
+'''
+multi line comments
+hii
+there
+kaise
+hai 
+aap 
+sab
+'''
+
+print("hii",6,78.45,sep=" -?- ",end="end")
+# by default , sep(seperator) = " ", end = "\n"
+
+print(" kaise hai aab sab")
