46232a5862cba819ee0d50ca2f7d2a5994ca19a1
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-03 14:08:07 +0530

    day 11

---
+++ day 011/main.py
@@ -0,0 +1,15 @@
+str1 = 'jiten'
+print(str1)
+print(str1[0])
+print(str1[2])
+print(str1[-1])
+
+str2 = """hii
+i
+am
+jitndriya meher"""
+print(str2)
+
+print("str2 =",end=" ")
+for ch in str2:
+    print(ch,end="")
