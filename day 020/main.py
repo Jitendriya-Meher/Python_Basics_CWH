f020a27cc9a565b315c5db2584fdeb704ebd59cd
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-04 06:37:16 +0530

    day 20

---
+++ day 020/main.py
@@ -0,0 +1,15 @@
+def calcGmean( a, b):
+    mean = (a+b)/2
+    print("mean =",mean)
+    if( a > b):
+        print("greter no =",a)
+    else:
+        print("greter no =",b)
+
+def lesser( a, b):
+    pass
+
+calcGmean(3,4)
+calcGmean(5,6)
+calcGmean(18,89)
+lesser(1,2)
