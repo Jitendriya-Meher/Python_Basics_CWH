4282f1642caad5602f6612802d5be8fd336ed7c9
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-04 12:03:17 +0530

    day 34

---
+++ day 034/main.py
@@ -0,0 +1,22 @@
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info)
+
+info.update({'age':20})
+info.update({'DOB':2001})
+print(info)
+
+info.clear()
+print(info)
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+info.pop('eligible')
+print(info)
+
+info.popitem()
+print(info)
+
+info = {'name':'Karan', 'age':19, 'eligible':True, 'DOB':2003}
+del info['age']
+print(info)
+
+del info
