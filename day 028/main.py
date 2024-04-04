aeb447072a49361effbee9e180e4dae916bcdd8a
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-04 08:13:41 +0530

    day 28

---
+++ day 028/main.py
@@ -0,0 +1,10 @@
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 49))
+
+val = 'Geeks'  
+print(f"{val}for{val} is a portal for {val}.")  
+name = 'Tushar'  
+age = 23  
+print(f"Hello, My name is {name} and I'm {age:.3f} years old.")  
+
+print(f"Hello, My name is {{name}} and I'm {{age}} years old.") 
