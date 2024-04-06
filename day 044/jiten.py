fa89338e36d84132654a056a6f59472660d28af2
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-06 08:45:49 +0530

    edit day 44

---
+++ day 044/jiten.py
@@ -0,0 +1,4 @@
+def hii():
+    print("Hii i am jitendriya...")
+
+jit = "Hii There"
---
+++ day 044/main.py
@@ -0,0 +1,20 @@
+import math as M
+# from math import *
+from math import sqrt as sq, pi
+
+import jiten as JIT
+
+a = M.floor(432.133)
+print(a)
+
+b = sq(38)
+print(b)
+
+print(pi)
+
+# print(dir(M))
+
+print(dir(JIT))
+
+JIT.hii()
+print(JIT.jit)
