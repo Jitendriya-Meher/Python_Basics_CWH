852a0b7af9c2716b053d1913139590ef6714f798
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-06 07:36:36 +0530

    edit ady 36

--- day 036/main.py
+++ day 036/main.py
@@ -1,13 +1,13 @@
-a = input("Enter the number: ")
-print(f"Multiplication table of {a} is: ")
-try:
-  for i in range(1, 11):
-    print(f"{int(a)} X {i} = {int(a)*i}")
-except:
-  print("Invalid  Input!")
 
-print("Some imp lines of code")
-print("End of program")
+try:
+  a = int(input("Enter the number: "))
+  print(f"Multiplication table of {a} is: ")
+  for i in range( 1, 11):
+    print(f"{a} X {i} = {a*i}")
+except Exception as e:
+  print("error =",e)
+
+print("End of Mutliplication table")
 
 try:
     num = int(input("Enter an integer: "))
