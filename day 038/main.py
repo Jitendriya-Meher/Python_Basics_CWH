e64f0b439f871cb1fea8e6b687dbccd13217e059
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-06 07:54:54 +0530

    edit day 38

--- day 038/main.py
+++ day 038/main.py
@@ -5,14 +5,18 @@ class CustomError(Exception):
 
 def my_function():
     a = int(input("Enter any value between 5 and 9 : "))
+    if( a<0):
+        raise ValueError("Value should be positive")
     if(a<5  or a>9):
         raise  CustomError("Value should be between 5 and 9")
-    else:
-        raise CustomError("Number is valid")
-
-
+    
 try:
     my_function()
+    print("No errors found")
 except CustomError as e:
+    print(e)
+except ValueError as e:
+    print(e)
+except Exception as e:
     print(e)
 
